7e7c6069ace3517f9023cad589aacc9cc6cc2adf
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-06 10:39:50 -0400

    rxercise 1 function

---
+++ demos/object.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+let employee = {
+    employeeId: "A132l",
+    name: "Ezra",
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+};
+console.log(employee.employeeId, employee.jobTitle, employee.name);
+console.log(employee.payRate);
