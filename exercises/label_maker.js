0bee2a337e09d068407eb06f372a2952cfea6552
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-06 11:47:19 -0400

    label 1

--- exercises/label_maker.js
+++ exercises/label_maker.js
@@ -1,19 +1,19 @@
 "use strict";
 
-function printContact(myInfo) {
-    console.log(myInfo.name);
-    console.log(myInfo.address);
-    console.log(myInfo.city);
-    console.log(myInfo.state);
-    console.log(myInfo.zip);
-}
-
 let myInfo = {
-    name: "Pooja",
-    address: "123 pl",
-    city: "Bloomfield",
-    state: "New jersey",
-    zip: 123456,
+  name: "Pooja",
+  address: "123 pl",
+  city: "Bloomfield",
+  state: "New jersey",
+  zip: 123456,
+};
+
+function printContact(myInfo) {
+  console.log(myInfo.name);
+  console.log(myInfo.address);
+  console.log(myInfo.city);
+  console.log(myInfo.state);
+  console.log(myInfo.zip);
 };
 
 printContact(myInfo);
