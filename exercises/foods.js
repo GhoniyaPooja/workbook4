96b0010c431c2de9527b2be6a53b934d7ba7211a
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-07 14:24:26 -0400

    exercise 3 food sub total

---
+++ exercises/foods.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let lunch = [
+    {item: "Steak Fajitas", price: 9.95},
+    {item: "Chips and Guacamole", price: 5.25},
+    {item: "Sweet Tea", price: 2.79}
+    ];
+let subtotal = 0;
+for (let i = 0; i < lunch.length; i++) {
+    subtotal += lunch[i].price;
+    
+}   
+
+let taxRate = 0.08;
+let tax = subtotal * taxRate;
+let tipRate = 0.18;
+let tip = subtotal * tipRate;
+let totalDue = subtotal + tax + tip
+
+console.log("The subtotal of lunch: " + subtotal);
+console.log("Adding 8% tax: " + tax);
+console.log("Adding 18% tip: " + tip);
+console.log("The total due is: " + totalDue);
