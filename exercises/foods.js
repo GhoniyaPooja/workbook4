58dde008b7029110ba17d10dfa8f41c535e35572
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-07 14:50:49 -0400

    add the console log

--- exercises/foods.js
+++ exercises/foods.js
@@ -1,21 +1,20 @@
 "use strict";
 
 let lunch = [
-    {item: "Steak Fajitas", price: 9.95},
-    {item: "Chips and Guacamole", price: 5.25},
-    {item: "Sweet Tea", price: 2.79}
-    ];
+  { item: "Steak Fajitas", price: 9.95 },
+  { item: "Chips and Guacamole", price: 5.25 },
+  { item: "Sweet Tea", price: 2.79 },
+];
 let subtotal = 0;
 for (let i = 0; i < lunch.length; i++) {
-    subtotal += lunch[i].price;
-    
-}   
+  subtotal += lunch[i].price;
+}
 
 let taxRate = 0.08;
 let tax = subtotal * taxRate;
 let tipRate = 0.18;
 let tip = subtotal * tipRate;
-let totalDue = subtotal + tax + tip
+let totalDue = subtotal + tax + tip;
 
 console.log("The subtotal of lunch: " + subtotal);
 console.log("Adding 8% tax: " + tax);
