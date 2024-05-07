d19f015cce2988f0aead04145530fad9182a3d99
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-07 13:13:30 -0400

    exercise 1 my family

--- demos/firstnames_array.js
+++ demos/firstnames_array.js
@@ -2,3 +2,7 @@
 
 let firstNames = ["Nedret", "Chris", "Nathaniel", "Yusuf"];
 console.log(firstNames[1]); //Chris
+
+for (let i = 0; i < firstNames.length; i++){
+    console.log(firstNames[i]);
+}
---
+++ exercises/my_family.js
@@ -0,0 +1,5 @@
+let familyNames = ["Pooja", "Anjli", "Dhaval", "Dhruv"];
+
+for (let i = 0; i < familyNames.length; i++){
+    console.log(familyNames[i]);
+}
