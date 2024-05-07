cbb842a5afc1698992d72eaaa82419e112cb94eb
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-07 13:59:05 -0400

    exercise 2 using function your scores

--- exercises/avg_scores.js
+++ exercises/avg_scores.js
@@ -3,11 +3,11 @@
 let myScores = [92, 98, 84, 76, 89, 99, 100];
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 let sum = 0;
-function getAverage(myScores) {
-  for (let i = 0; i < myScores.length; i++) {
-    sum += myScores[i];
+function getAverage(scores) {
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
   }
-  return sum / myScores.length;
+  return sum / scores.length;
 }
-let myAverage = getAverage(myScores);
+let myAverage = getAverage(yourScores);
 console.log("My average score is: " + myAverage);
