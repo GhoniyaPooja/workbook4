c014a889cd29ccbb96f1f5ce7c27d71a62e55fec
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-07 10:20:02 -0400

    do while

---
+++ exercises/while_loops.js
@@ -0,0 +1,18 @@
+"use strict";
+//using while loop
+let i = 0;
+let text = "I Love loops";
+while (i < 7) {
+    text = text 
+    console.log(text);
+    i++;
+}
+
+//using do while loop
+let index = 0;
+let text1 = "I love loops";
+
+do {
+    console.log(text1);
+    index++;
+} while (index < 7);
