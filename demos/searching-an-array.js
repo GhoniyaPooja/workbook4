6d104b514771a1710e13a85dddcf354ba2d87cac
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-08 09:54:18 -0400

    searching an array

---
+++ demos/searching-an-array.js
@@ -0,0 +1,21 @@
+"use stict";
+
+//indexOf on string
+// let teamName = "New York Knicks";
+// let position = teamName.indexOf("z")
+// console.log(position);
+
+// if(position == -1){
+//     console.log("Not found");
+// }
+
+//indexOf on array of strings
+let teams = [
+  "New York Kicks",
+  "Indiana Pacers",
+  "Mamphis Grizzlies",
+  "Dever Nuggets",
+];
+
+let positionInArray = teams.indexOf("Mamphis Grizzlies");
+console.log(positionInArray);
