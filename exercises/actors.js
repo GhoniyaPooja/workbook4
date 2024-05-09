539909caf5c46af06043d33afdf420c7742846a7
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-05-09 14:40:44 -0400

    exercises

--- exercises/actors.js
+++ exercises/actors.js
@@ -26,3 +26,34 @@ let academyMembers = [
    ];
    
    // Who is the Academy Member whose ID is 187?
+   let targetID = 187;
+let targetMember = null;
+
+for (let i = 0; i < academyMembers.length; i++) {
+  if (academyMembers[i].memID == targetID) {
+    targetMember = academyMembers[i];
+    break;
+  }
+}
+console.log(targetMember.name);
+
+   // Who has have been in at least 3 films?
+for (let i = 0; i < academyMembers.length; i++) {
+  if (academyMembers[i].films.length >= 3) {
+    console.log(academyMembers[i].name);
+  }
+}
+
+// Who has a name that starts with "Bob"?
+for (let i = 0; i < academyMembers.length; i++) {
+  if (academyMembers[i].name.startsWith("Bob")) {
+    console.log(academyMembers[i].name);
+  }
+}
+//Which Academy Members have been in a film that starts with "A"
+for (let i = 0; i < academyMembers.length; i++) {
+    if (academyMembers[i].films.startsWith("A")) {
+        console.log(academyMembers[i].name);
+    }
+    
+}
